881facfad91826d381f1536ab65d0f755363d08a
wacats_wh <seowwenhan_96@hotmail.com>
2017-04-28 02:16:13 +0800

    connect and test to online mongodb database

--- api/controllers/appData.controllers.js
+++ api/controllers/appData.controllers.js
@@ -1,7 +1,18 @@
+var dbconn = require('../data/dbconnection.js');
 var appData = require('../data/app-data.json');
 
 // return app app data in the json
 module.exports.appDataGetAll = function(req, res) {
+
+	// **** this segment is to test if db connection is successful
+	// **** aware that the return data is not queried from the database
+
+	var db = dbconn.get();
+
+	console.log("testing database connection: ", db);
+
+	// **** test segment ends here
+
 	console.log('GET the App Data json');
 	res
 		.status(200)
