59793691ddb115dff0c9b5a8394fcda94b703642
wacats_wh <seowwenhan_96@hotmail.com>
2017-05-04 18:45:29 +0800

    app-data.json has been denied by wacatswh, clean up appData controller

--- api/controllers/appData.controllers.js
+++ api/controllers/appData.controllers.js
@@ -1,23 +1,9 @@
 var db = require('../data/db.js');
-// var appData = require('../data/app-data.json');
 var mongoose = require('mongoose');
 var App = mongoose.model('App');
 
-// return app data in the json
 module.exports.appDataGetAll = function(req, res) {
 
-	// **** this segment is to test if db connection is successful
-	// **** aware that the return data is not queried from the database
-
-	// console.log("testing database connection: ", db);
-
-	// **** test segment ends here
-    /*
-	console.log('GET the App Data json');
-	res
-		.status(200)
-		.json(appData);
-    */
     App
         .find()
         .exec(function(err, app) {
