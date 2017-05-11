6e816f9789cfb6c2d38d96f0e4c2e0873915b340
kalai <kalaichelvan7@yahoo.com>
2017-05-11 23:01:06 +0800

    removed head and adding some design

--- public/js/App.controller.js
+++ public/js/App.controller.js
@@ -1,11 +1,19 @@
 var PatioWeb = angular.module('PatioWeb', []);
 
 PatioWeb.controller('AppsController', ['$scope',' $http', '$location', '$routeParamsfunction', function($scope, $http, $location, $routeParams){
-    console.log('Appscontrller lodaead ....')
-
     $scope.getApps = function(){
         $http.get(api/appData).success(function(response){
             $scope.apps = response;
         });
     }
-}]);
+
+    $scope.toogle1 = true;
+    $scope.toogle2 = true;
+    $scope.toogle3 = true;
+    $scope.toogle4 = true;
+    $scope.toogle5 = true;
+    $scope.toogle6 = true;
+
+
+
+}]);    
